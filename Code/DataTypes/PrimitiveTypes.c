eb8359556c8141fe94f29ff17747702c71cb83f0
vedavith ravula <vedavithravula1996@gmail.com>
2023-02-24 05:11:29 +0530

    Primitives Example 1

---
+++ Code/DataTypes/PrimitiveTypes.c
@@ -0,0 +1,17 @@
+// print an integer, float and a charecter
+#include<stdio.h>
+void main() 
+{
+    int a  = 10; // +ve integer
+    int b = -10; // -ve integer
+    int c = 10U; // Unsigned integer
+    long int d = 99998L; // Long integer
+    float e = 10.20f; // Float
+    double f = 123123123.00;
+    double g = 12.293123;
+    double h = 2312312312.123123;
+    char i = 'a';
+    char j = 99;
+
+    // Print all the variables
+}
