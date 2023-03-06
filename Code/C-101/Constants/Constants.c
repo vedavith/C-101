67d9f761d5815c1e049ee79064920f6361e9f60f
vedavith ravula <vedavithravula1996@gmail.com>
2023-03-07 05:19:19 +0530

    constants

--- Code/C-101/Constants/Constants.c
+++ Code/C-101/Constants/Constants.c
@@ -10,10 +10,16 @@
 int main()
 {
     const int length = 10;
-    const int width = 20;
-    const int height = 30;
-    int area = 0;
-    area = lenght * width * height;
+    const int width = 1;
+    const int height = 1;
+    int area;
+    int lengthOverride = length;
+    lengthOverride++;
+    // length++; - cannot assign to variable 'length' with const-qualified type 'const int'
+    
+    area = lengthOverride * width * height;
     printf("%d\n", area);
     return 0;
+    
+    
 }
--- Code/C-101/Constants/DefinedConstants.c
+++ Code/C-101/Constants/DefinedConstants.c
@@ -5,16 +5,16 @@
 //  Created by Vedavith Ravula on 06/03/23.
 //
 
-#define LENGTH 10;
-#define WIDTH 20;
-#define HEIGHT 5;
+#define L 10;
+#define W 20;
 
 #include <stdio.h>
 
 int main()
 {
-    int area = 0;
-    area = LENGTH * WIDTH * HEIGHT;
-    printf("%d\n", area);
+    long area;
+    area = L * W;
+    printf("%ld\n", area);
     return 0;
 }
+
