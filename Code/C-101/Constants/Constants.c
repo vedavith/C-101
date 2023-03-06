29f9ec6b5b6d9684885c5955c275acb85e45059d
vedavith ravula <vedavithravula1996@gmail.com>
2023-03-06 20:10:35 +0530

    Constants

---
+++ Code/C-101/Constants/Constants.c
@@ -0,0 +1,19 @@
+//
+//  Constants.c
+//  C-101
+//
+//  Created by Vedavith Ravula on 06/03/23.
+//
+
+#include <stdio.h>
+
+void main()
+{
+    const int length = 10;
+    const int width = 20;
+    const int height = 30;
+    int area = 0;
+    area = lenght * width * height;
+    printf("%d\n", area);
+    return 0;
+}
---
+++ Code/C-101/Constants/DefinedConstants.c
@@ -0,0 +1,20 @@
+//
+//  DefinedConstants.c
+//  C-101
+//
+//  Created by Vedavith Ravula on 06/03/23.
+//
+
+#define LENGTH 10;
+#define WIDTH 20;
+#define HEIGHT 5;
+
+#include <stdio.h>
+
+void main()
+{
+    int area = 0;
+    area = LENGTH * WIDTH * HEIGHT;
+    printf("%d\n", area);
+    return 0;
+}
