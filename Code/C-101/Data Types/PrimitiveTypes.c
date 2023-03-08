7db21ed6a2f6a51b8bb1e682bbf62d10d7a9d599
vedavith ravula <vedavithravula1996@gmail.com>
2023-03-09 04:52:02 +0530

    Input-Scanf function

--- Code/C-101/Data Types/PrimitiveTypes.c
+++ Code/C-101/Data Types/PrimitiveTypes.c
@@ -9,17 +9,15 @@
 #include<stdio.h>
 void main()
 {
-    int a  = 10; // +ve integer
-    int b = -10; // -ve integer
+    int a  = 10; // Positive integer
+    int b = -10; // Negitive integer
     int c = 10U; // Unsigned integer
     long int d = 99998L; // Long integer
     float e = 10.20f; // Float
-    double f = 123123123.00;
-    double g = 12.293123;
-    double h = 2312312312.123123;
-    char i = 'a';
-    char j = 99;
-
-    // Print all the variables
+    double f = 123123123.00; // Double
+    double g = 12.293123; // Double
+    double h = 2312312312.123123; // Double
+    char i = 'a'; // Char
+    char j = 99; // Char
     printf("%d\n", a);
 }
---
+++ Code/C-101/ReadInput/Input.c
@@ -0,0 +1,17 @@
+//
+//  Input.c
+//  C-101
+//
+//  Created by Vedavith Ravula on 09/03/23.
+//
+
+#include <stdio.h>
+int main()
+{
+    int testInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger);
+    printf("Number = %d",testInteger);
+    return 0;
+}
+
