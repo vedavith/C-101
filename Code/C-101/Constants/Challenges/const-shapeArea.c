fd198dd3f450ee580f80497546e27f7bd71fbcb8
shishirr07 <shishirr07@gmail.com>
2023-03-08 18:36:35 -0500

    git

---
+++ Code/C-101/Constants/Challenges/const-shapeArea.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+#define side 5
+
+int main() {
+
+   int area;
+   area = side * side;
+   printf("square area : %d", area);
+
+   return 0;
+}
+
+/*
+#include <stdio.h>
+
+#define length 5
+#define width 4
+
+int main() {
+
+   int area;
+   area = length * width;
+   printf("rectangle area : %d", area);
+
+   return 0;
+}
+*/
+
