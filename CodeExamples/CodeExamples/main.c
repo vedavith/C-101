239d2694f0a7c503f0ea10d2ea32c57ac2a2625b
vedavith ravula <vedavithravula1996@gmail.com>
2023-03-13 22:41:15 +0530

    renamed folders and files

---
+++ CodeExamples/CodeExamples/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CodeExamples
+//
+//  Created by Vedavith Ravula on 13/03/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
