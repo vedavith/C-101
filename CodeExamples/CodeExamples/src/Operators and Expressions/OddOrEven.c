0d05dc8b395e08abea68ef2375d56242f8be7d5e
shishirr07 <shishirr07@gmail.com>
2023-03-29 09:40:03 -0400

    Odd or Even Challenge 3/28/2023

---
+++ CodeExamples/CodeExamples/src/Operators and Expressions/OddOrEven.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, c;
+    printf("Pick a number, I'll tell you if it is odd or even!");
+    scanf("%d", &a);
+
+    c = a % 2;
+
+    if (c = 0)
+    {
+        printf("Your number is even!");
+    }
+
+    else
+    {
+        printf("Your number is odd.");
+    }
+    
+    
+    
+}
