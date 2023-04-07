965ab3518dabce8e076f1176c5ec542d6142a0f2
shishirr07 <shishirr07@gmail.com>
2023-04-06 21:43:02 -0400

    My operators program

---
+++ CodeExamples/CodeExamples/src/Operators and Expressions/Operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int key, answer1 = 5028, answer2 = 5030;
+    printf("Enter your first key to unlock the padlock.(4 digits)");
+    scanf("%d", &key);
+
+    if ((key < 1000) && (key > 9999)) {
+        printf("Invalid key. Please try again later.");
+        return 0;
+    }
+    else if (key != answer1) {
+        printf("Unfortunately, your answer is wrong. Try again later.");
+    }
+    else if ((key == answer1) || (key == answer2)) {
+        printf("Good job! you have unlocked the locker!");
+    }
+    
+
+}
