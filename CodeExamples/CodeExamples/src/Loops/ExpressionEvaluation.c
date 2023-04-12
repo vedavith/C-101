b3e382f135b2934859c26d18fa25ba597a078668
vedavith ravula <vedavithravula1996@gmail.com>
2023-04-13 05:12:10 +0530

    Task: ExpressionEvaluation

--- CodeExamples/CodeExamples/src/Loops/ExpressionEvaluation.c
+++ CodeExamples/CodeExamples/src/Loops/ExpressionEvaluation.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    int count, n;
+    float x, y;
+    printf("Enter the values of x and n :");
+    scanf("%f %d", &x, &n);
+
+    y = 1.0; // initialize
+    count = 1;
+    while (count <= n) { // Test
+        y = y * x;
+        count++;
+    }
+    
+    printf("\nx = %f; n = %d; x^n = %f\n", x, n, y);
     return 0;
 }
