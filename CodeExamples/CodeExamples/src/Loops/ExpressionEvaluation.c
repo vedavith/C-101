8ec460390a9a1f630a421911ee399b46e2ae1cfd
shishirr07 <shishirr07@gmail.com>
2023-04-15 23:18:44 -0400

    Completed Powers Challenge

--- CodeExamples/CodeExamples/src/Loops/ExpressionEvaluation.c
+++ CodeExamples/CodeExamples/src/Loops/ExpressionEvaluation.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+// Write a program to evaluate the equation y=x^n, where n is a non-negitive integer.
+int main() {  
+    long long x, y = 1, n, count = 1;
+    printf("Please give a number:");
+    scanf("%lld", &x);
 
-int main() {
-    return 0;
+    printf("Please give another number:");
+    scanf("%lld", &n);
+    
+    while(count <= n) {
+        y = x * y;
+        count++;
+    }
+
+    printf("%lld", y);
+
+
+   
+
+
 }
