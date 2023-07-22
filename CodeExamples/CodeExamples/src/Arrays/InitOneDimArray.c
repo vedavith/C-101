cbcf97199b4e24cf1c8c3655847132f9081788c7
vedavith ravula <vedavithravula1996@gmail.com>
2023-07-22 06:49:10 +0530

    Initialization of arrays

---
+++ CodeExamples/CodeExamples/src/Arrays/InitOneDimArray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// driver
+int main() {
+    // compile initialization - int array-name[size] = {list of values};
+    //int number[4] = {1, 2, 3, 4};
+
+    // run time initialization - We initialize the array at the run time
+    int numbers[5];
+    for (int j = 0; j < 5; j++) {
+        numbers[j] = j+1;
+    }
+
+    //int number[] = {1, 2}; //
+    // Printing the array values
+    int size = sizeof(numbers)/sizeof(numbers[0]);
+    for (int i = 0; i < size; i++) {
+        printf("number[%d] = %d\n", i, numbers[i]);
+    }
+}
